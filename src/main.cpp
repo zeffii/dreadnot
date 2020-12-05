9f3b369287148b82db2529fd668d7072b844f401
zeffii <tetha.z@gmail.com>
2020-12-05 23:16:06 +0100

    add window class

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <SDL2/SDL.h>
-// #include <SDL2/SDL_image.h>
+//#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <vector>
 #include <string>
 
 #include "Font_Parser.h"
+#include "window.h"
 
+
+void pollEvents(Window &window){
+    SDL_Event event;
+
+    if (SDL_PollEvent(&event)){
+        window.pollEvents(event);
+    }
+}
 
 int main(int argc, char* args[])
 {
-    // read https://lazyfoo.net/tutorials/SDL/41_bitmap_fonts/index.php
-    Font_Parser font("res/consolas.fnt");
-    // font.print_glyph_info("p");
 
-    
-    while(1){}
+    Font_Parser font("res/consolas.fnt");
+    Window window( "SDL Tutorial", 800, 600);
+
+    while (!window.isClosed()){
+        pollEvents(window);
+        window.clear();
+    }
     return 0;
 }
---
+++ src/window.cpp
@@ -0,0 +1,83 @@
+//window.cpp
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <iostream>
+#include "Window.h"
+using namespace std;
+
+SDL_Renderer *Window::renderer = nullptr;
+
+Window::Window(const string &title, int width, int height)
+:_title(title), _width(width), _height(height){
+    _closed = !init();   
+}
+
+Window::~Window(){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(_window);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+bool Window::init(){
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+        cerr << "Failed to init SDL.\n";
+        return false;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG){
+        cerr << "Failed to init SDL img\n";
+        return false;
+    }
+
+    _window = SDL_CreateWindow(
+        _title.c_str(),
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        _width, _height,
+        0  // SDL_WINDOW_RESIZABLE
+    );
+
+    if (_window == nullptr) {
+        cerr << "Failed to create window! :/ \n";
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == nullptr) {
+        cerr << "Failed to create renderer!\n";
+        return false;
+    }
+
+    return true;
+}
+
+void Window::pollEvents(SDL_Event &event){
+    switch (event.type){
+        case SDL_QUIT:
+            _closed = true;
+            break;
+        case SDL_KEYDOWN:
+                
+            switch (event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    _closed = true;
+                    break;
+                default:
+                    break;                    
+            }
+            break;
+
+         default:
+            break;
+    }
+}
+
+void Window::clear() const {
+    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 125, 125, 125, 255);
+    SDL_RenderClear(renderer);
+
+}
